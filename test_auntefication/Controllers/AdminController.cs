2473599de299ec0e9fa265fc1ad27158106418fc
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-09-23 16:26:42 +0300

    Добавьте файлы проекта.

---
+++ test_auntefication/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using test_auntefication.Models;
+using test_auntefication.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace test_auntefication.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+        public AdminController(UserManager<IdentityUser> usrMng, RoleManager<IdentityRole> roleMng)
+        {
+            userManager = usrMng;
+            roleManager = roleMng;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+        public ViewResult ListRole() => View(roleManager.Roles.ToList());
+        public ViewResult Edit() => View();
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Edit( string email)
+        {
+            string name = User.Identity.Name;
+            IdentityUser user = await userManager.FindByNameAsync(name);
+            if (user != null)
+            {
+                user.Email = email;
+                IdentityResult result = await userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View("Error", "Erorr");
+        }
+    }
+}
---
+++ test_auntefication/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using test_auntefication;
+
+namespace test_auntefication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
